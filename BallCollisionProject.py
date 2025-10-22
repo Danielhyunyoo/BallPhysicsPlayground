21814224d394f7c28a6b70afc0d204a81caa6938
Daniel Yoo <145076648+Danielhyunyoo@users.noreply.github.com>
2025-10-21 20:24:26 -0500

    Updated BallCollisionProject.py

--- BallCollisionProject.py
+++ BallCollisionProject.py
@@ -1,13 +1,15 @@
 import pygame
 import sys
 
+# Importing other local files
+from box import Box
 
 # Initializing Pygame
 pygame.init()
 
-# Creating our playground window
-Width = 1000
-Height = 800
+# Creating our playground window -> 1600 x 900 Resolution -> HD+
+Width = 1600
+Height = 900
 
 screen = pygame.display.set_mode((Width, Height))
 
@@ -17,9 +19,14 @@ pygame.display.set_caption("2D Ball Physics Box Playground")
 # Managing frame rates
 clock = pygame.time.Clock()
 
+# Making box object
+box = Box(Width, Height, margin=150)
 
 # Window set up
 window_running = True
+
+# Color themeing
+bg_color = (37, 45, 64)
 
 while window_running:
     for event in pygame.event.get():
@@ -27,8 +34,12 @@ while window_running:
             window_running = False
             
     # Adding background color -> Dark-ish gray
-    screen.fill((67, 67, 67))
+    screen.fill(bg_color)
     
+    # Drawing our box
+    box.draw(screen)
+    
+    # Updating the screen
     pygame.display.flip()
     
     # Adding FPS limiter -> 165 FPS for my 165 Hz monitor
